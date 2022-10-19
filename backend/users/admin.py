b1404598af67754c8db3cacac039763d02b1ea59
Evgeniy Kazakov <junglecommunity@gmail.com>
2022-10-19 16:54:22 +0700

    init class user fix, basic config setting.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,3 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib import admin
 
-
-class User(AbstractUser):
-    """Модель пользователя"""
-    pass
+# Register your models here.
