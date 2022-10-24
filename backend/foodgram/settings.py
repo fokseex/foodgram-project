214b2240f0c1c157e8818cb98f100e7ba4509812
Evgeniy Kazakov <junglecommunity@gmail.com>
2022-10-24 17:34:21 +0700

    pep8 fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'DB_ENGINE': os.getenv('POSTGRES_DB',
-                          default='django.db.backends.postgresql'),
+                               default='django.db.backends.postgresql'),
         'NAME': os.getenv('POSTGRES_DB', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='12345678'),
